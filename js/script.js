95c11e9db87235f3e50962c9acab2b83554fc5e9
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-14 17:11:46 +0200

    Add && condition

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 let Grid = document.querySelector('.container-grid');
 
-for(let i = 1; i <= 100; i++) {
-    console.log('i');
+//  for(let i = 1; i <= 100; i++) {
 
-    if (i % 15 == 0) {
+//     if (i % 15 == 0) {
+//         Grid.innerHTML = Grid.innerHTML + `<div class="square fizzbuzz">fizzbuzz</div>`;
+//     } else if (i % 5 == 0) {
+//         Grid.innerHTML = Grid.innerHTML + `<div class="square buzz">buzz</div>`;
+//     } else if (i % 3 == 0) {
+//         Grid.innerHTML = Grid.innerHTML + `<div class="square fizz">Fizz</div>`;
+//     } else {
+//         Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
+
+//     }
+//  }
+
+ for(let i = 1; i <= 100; i++) {
+
+    if (i % 5 == 0) && (i % 3 == 0) {
         Grid.innerHTML = Grid.innerHTML + `<div class="square fizzbuzz">fizzbuzz</div>`;
     } else if (i % 5 == 0) {
         Grid.innerHTML = Grid.innerHTML + `<div class="square buzz">buzz</div>`;
@@ -13,4 +26,4 @@ for(let i = 1; i <= 100; i++) {
         Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
 
     }
-}
+ }
