9dd5372e69f643e8f98ff6b71226a22bf0cec994
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-14 16:02:41 +0200

    Add js cicle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let Grid = document.querySelector('.container-grid');
 
-for(let i = 0; i >= 100; i++) {
+for(let i = 0; i <= 100; i++) {
     console.log('i');
     Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
 }
