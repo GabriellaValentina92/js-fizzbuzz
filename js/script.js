aa3bc0aad48030ba700e1d0344fb1ca9dd8b2472
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-14 16:57:49 +0200

    Add square-color/name

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let Grid = document.querySelector('.container-grid');
 
-for(let i = 0; i <= 100; i++) {
+for(let i = 1; i <= 100; i++) {
     console.log('i');
 
     if (i % 15 == 0) {
-        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
+        Grid.innerHTML = Grid.innerHTML + `<div class="square fizzbuzz">fizzbuzz</div>`;
     } else if (i % 5 == 0) {
-        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i} buzz</div>`;
+        Grid.innerHTML = Grid.innerHTML + `<div class="square buzz">buzz</div>`;
     } else if (i % 3 == 0) {
-        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i} Fizz</div>`;
+        Grid.innerHTML = Grid.innerHTML + `<div class="square fizz">Fizz</div>`;
     } else {
         Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
 
