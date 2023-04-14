14665dd2c45a2aaa14883b1f3201a62fda02d839
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-14 16:22:34 +0200

    Add if-elseif

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ let Grid = document.querySelector('.container-grid');
 
 for(let i = 0; i <= 100; i++) {
     console.log('i');
-    Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
+
+    if (i % 15 == 0) {
+        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
+    } else if (i % 5 == 0) {
+        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i} buzz</div>`;
+    } else if (i % 3 == 0) {
+        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i} Fizz</div>`;
+    } else {
+        Grid.innerHTML = Grid.innerHTML + `<div class="square">${i}</div>`;
+
+    }
 }
